b2ac336eca08fb7784fca0c0e54fc41e0a6db2e7
Jarek Strzelecki <jarst@users.noreply.github.com>
2021-11-27 20:34:59 +0100

    Import project files

---
+++ src/main/groovy/io/github/jarst/springgroovy/SpringGroovyApplication.groovy
@@ -0,0 +1,13 @@
+package io.github.jarst.springgroovy
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringGroovyApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SpringGroovyApplication, args)
+	}
+
+}
